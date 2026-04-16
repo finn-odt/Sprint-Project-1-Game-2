b33420b3e171525fd30b162b5d0de3171cb3e461
finn-odt <finn@joker-s.de>
2026-04-16 21:24:15 +0200

    Modified Door-Animation-Controller | Added open Colliders | finished Gate Prefab

--- Game-2/Assets/Scripts/Interactables/Gate.cs
+++ Game-2/Assets/Scripts/Interactables/Gate.cs
@@ -1,18 +1,49 @@
+using System.Collections;
 using TriInspector;
 using UnityEngine;
 
 public class Gate : MonoBehaviour, IInteractable
 {
-    [SerializeField, LabelText("Animator for Gate")] private Animator animator;
+    private Animator gateAnimator;
+    [SerializeField, LabelText("Door Collider")] private BoxCollider doorCollider;
+
+    private void Start()
+    {
+        gateAnimator = GetComponentInChildren<Animator>();
+    }
 
     public void Interact(GameObject player)
     {
         // TODO: somehow start animation of player for lock picking
 
-        // remove Layer for clearing interactability
-        gameObject.layer = LayerMask.NameToLayer("UsedInteractable");
 
         // TODO: start animation of gate swinging open
+        if(gateAnimator != null) {
+            gateAnimator.SetTrigger("openGate");
+            StartCoroutine(WaitForIdleOpen());
+        }
 
+        // remove Layer for clearing interactability
+        gameObject.layer = LayerMask.NameToLayer("UsedInteractable");
+    }
+
+    private IEnumerator WaitForIdleOpen()
+    {
+        // wait one frame
+        yield return null;
+
+        while (gateAnimator != null)
+        {
+            AnimatorStateInfo state = gateAnimator.GetCurrentAnimatorStateInfo(0);
+
+            if (!gateAnimator.IsInTransition(0) && state.IsName("idle_open"))
+            {
+                // gate is now fully open -> deactivate collider to go through
+                doorCollider.enabled = false;
+                yield break;
+            }
+
+            yield return null;
+        }
     }
 }
